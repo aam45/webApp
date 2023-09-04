9275bd0c64814ea263a58b0f12ec38a59f94d693
aminkan24 <aminkan45@web.de>
2023-09-04 21:19:04 +0200

    Nr. 6-make pages active

--- src/app/pages/controls/controls.component.ts
+++ src/app/pages/controls/controls.component.ts
@@ -7,25 +7,32 @@ import { NavigationService } from '../../core/navigation.service';
   styleUrls: ['./controls.component.css'],
 })
 export class ControlsComponent {
+  selectedTab: string = 'home';
+
   constructor(private readonly navigationService: NavigationService) {}
 
   public navigateToHome() {
+    this.selectedTab = 'home';
     this.navigationService.home();
   }
 
   public navigateToAbout() {
+    this.selectedTab = 'about';
     this.navigationService.about();
   }
 
   public navigateToPortfolio() {
+    this.selectedTab = 'portfolio';
     this.navigationService.portfolio();
   }
 
   public navigateToBlogs() {
+    this.selectedTab = 'blogs';
     this.navigationService.blogs();
   }
 
   public navigateToContact() {
+    this.selectedTab = 'contact';
     this.navigationService.contact();
   }
 
